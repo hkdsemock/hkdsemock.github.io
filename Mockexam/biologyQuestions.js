febaf3de6989e7e7b2262c91c7cf7b2b64f7c5f2
hkdsemock <novaacademy.hkdse@gmail.com>
2025-02-17 23:11:39 +0800

    Add files via upload

---
+++ Mockexam/biologyQuestions.js
@@ -0,0 +1,118 @@
+// This file exports an array of question objects for the Biology subject.
+const biologyQuestions = [
+  {
+    question: "Question 1 (Level 3)",
+    description: "Which of the following regarding aerobic respiration in muscle is correct?",
+    options: {
+      A: "Aerobic respiration in muscle cells produces more ATP than anaerobic respiration.",
+      B: "Lactic acid is a byproduct of aerobic respiration in muscle cells.",
+      C: "Oxygen is not required for aerobic respiration to occur in muscle cells.",
+      D: "Aerobic respiration in muscle cells involves the Krebs cycle and electron transport chain."
+    },
+    correct: "D"
+  },
+  {
+    question: "Question 2 (Level 3)",
+    description: "Which type of cell division results in the formation of gametes?",
+    options: {
+      A: "Mitosis",
+      B: "Meiosis",
+      C: "Fertilization",
+      D: "Binary fission"
+    },
+    correct: "B"
+  },
+  {
+    question: "Question 3 (Level 3)",
+    description: "Which of the following is an example of an autotroph?",
+    options: {
+      A: "Zebra",
+      B: "Mushroom",
+      C: "Grass",
+      D: "Homosapiens"
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 4 (Level 4)",
+    description: "Which of the following is a requirement for natural selection to occur?",
+    options: {
+      A: "Genetic variation",
+      B: "Random mating",
+      C: "Stable environment",
+      D: "High mutation rate"
+    },
+    correct: "A"
+  },
+  {
+    question: "Question 5 (Level 4)",
+    description: `What of the following are the roles of hydrochloric acid in stomach?
+    
+    <div class="statement-list">
+      (1) Creates an acidic environment that kills potentially harmful bacteria in ingested food.<br>
+      (2) Activate pepsin to break down proteins.<br>
+      (3) Breaks down carbohydrates into simple sugars.<br>
+      (4) Emulsify fat into tiny oil droplet
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(2) and (3) only",
+      C: "(2), (3), (4) only",
+      D: "All of the above"
+    },
+    correct: "A"
+  },
+  {
+    question: "Question 6 (Level 4)",
+    description: "What is the role of tRNA in protein synthesis?",
+    options: {
+      A: "Carries amino acids to the ribosome",
+      B: "Transcribes DNA into mRNA",
+      C: "Initiates translation",
+      D: "Synthesizes proteins"
+    },
+    correct: "D"
+  },
+  {
+    question: "Question 7 (Level 5)",
+    description: `Which of the following are adaptive feature of air sacs for gas exchange?
+    
+    <div class="statement-list">
+      (1) Air sacs have thin wall<br>
+      (2) Air sacs have a rich supply of capillaries<br>
+      (3) Air sacs can be repaired and replenished quickly if damaged<br>
+      (4) There are a large number of air sacs in our lungs
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(1), (2) and (3) only",
+      C: "(1), (2) and (4) only",
+      D: "All of the above"
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 8 (Level 5)",
+    description: "Which of the following organs aid in the digestion and absorption of fats?",
+    options: {
+      A: "Liver, Pancreas, Large intestine",
+      B: "Stomach, Pancreas, Large intestine",
+      C: "Liver, Gallbladder, Pancreas",
+      D: "Liver, Gallbladder, Large intestine"
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 9 (Level 5)",
+    description: "What is the significance of the Krebs cycle in cellular respiration?",
+    options: {
+      A: "Production of ATP",
+      B: "Generation of carbon dioxide",
+      C: "Oxidation of glucose",
+      D: "Regeneration of electron carriers"
+    },
+    correct: "D"
+  }
+];
+
+export default biologyQuestions; 
---
+++ Mockexam/economyQuestions.js
@@ -0,0 +1,128 @@
+// This file exports an array of question objects for the Economy subject.
+const economyQuestions = [
+  {
+    question: "Question 1",
+    description: `When fans queue for 24 hours for the concert tickets, this implies that
+    
+    <div class="statement-list">
+      (1) The concert tickets are economic goods.<br>
+      (2) There is competition.<br>
+      (3) The concert tickets are limited supply.
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(1) and (3) only",
+      C: "(2) and (3) only",
+      D: "(1), (2) and (3)"
+    },
+    correct: "A"
+  },
+  {
+    question: "Question 2",
+    description: `Which of the following will be the causes of a change in the demand for a good?
+    
+    <div class="statement-list">
+      (1) An increase in its unit market price<br>
+      (2) A decrease in consumers' income<br>
+      (3) An increase in the prices of substitutes
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(1) and (3) only",
+      C: "(2) and (3) only",
+      D: "(1), (2) and (3)"
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 3",
+    description: "'I go to the café every day to buy a cup of coffee, even though the price of the coffee has doubled,' Susan said. Susan's demand for coffee is",
+    options: {
+      A: "perfectly inelastic.",
+      B: "perfectly elastic.",
+      C: "unitary elastic.",
+      D: "inelastic."
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 4",
+    description: "After the cancellation of a subsidy for electronic products, the market price of these products will ____________ and the quantity transacted will _____________.",
+    options: {
+      A: "increase … increase",
+      B: "increase … decrease",
+      C: "decrease … increase",
+      D: "decrease … decrease"
+    },
+    correct: "B"
+  },
+  {
+    question: "Question 5",
+    description: "When unanticipated deflation occurs, which of the following persons will lose?",
+    options: {
+      A: "Carrie, who holds bonds from a listed company which pay a regular fixed amount of interest income.",
+      B: "Vivian, who rents a machine at a fixed monthly rental payment",
+      C: "Felix, who holds all his assets in cash",
+      D: "Kenneth, who received a fixed monthly salary"
+    },
+    correct: "B"
+  },
+  {
+    question: "Question 6",
+    description: "Suppose the Government adopts the following policies. Which of the following policies will cause the price level to decrease in the short run?",
+    options: {
+      A: "The government lowers the profit tax rate.",
+      B: "The government invests in infrastructure.",
+      C: "The government employs more temporary staff.",
+      D: "The government reduces the salaries tax allowances."
+    },
+    correct: "D"
+  },
+  {
+    question: "Question 7",
+    description: "The population of a country increases by 4%, while its price level and nominal GDP decrease by 3% and 2%, respectively. This implies that the real GDP _______ and per capita real GDP _______.",
+    options: {
+      A: "increases … increases",
+      B: "increases … decreases",
+      C: "decreases … decreases",
+      D: "decreases … increases"
+    },
+    correct: "B"
+  },
+  {
+    question: "Question 8",
+    description: `Efficiency is attained when
+    
+    <div class="statement-list">
+      (1) Profit is maximised.<br>
+      (2) Total social surplus is maximised.<br>
+      (3) Marginal social benefit equals marginal social cost.
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(1) and (3) only",
+      C: "(2) and (3) only",
+      D: "(1), (2) and (3)"
+    },
+    correct: "C"
+  },
+  {
+    question: "Question 9",
+    description: `Which of the following is/are the functions of price?
+    
+    <div class="statement-list">
+      (1) To distribute goods to the consumers<br>
+      (2) To allocate resources in markets<br>
+      (3) To solve the problem of income inequality
+    </div>`,
+    options: {
+      A: "(1) and (2) only",
+      B: "(1) and (3) only",
+      C: "(2) and (3) only",
+      D: "(1), (2) and (3)"
+    },
+    correct: "A"
+  }
+];
+
+export default economyQuestions; 
